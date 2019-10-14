bbe3a9f9e65e9e6267e1985b6995047332d9c1a7
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-14 13:08:15 +0100

    display tests written

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Display locked={false} closed={false}  />);
+});
+
+describe('Display component', () => {
+
+  test('matches the snapshot!', () => {
+    expect(wrapper.container).toMatchSnapshot();
+  });
+
+  it('display is set to open and unlocked by default', () => {
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+  });
+});
